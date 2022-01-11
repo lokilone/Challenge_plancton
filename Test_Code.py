40dca5da66d9ed03f7c3d1e13ff0e64f54613eb7
GasserMarc <45004970+GasserMarc@users.noreply.github.com>
2022-01-11 14:31:25 +0100

    Create Test_Code.py

---
+++ Test_Code.py
@@ -0,0 +1,191 @@
+import torch
+import torchvision                                                       
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+import os.path
+
+
+dataset_dir = os.path.join(os.path.expanduser("~"), 'Datasets', 'FashionMNIST')
+valid_ratio = 0.2  #
+
+# Load the dataset for the training/validation sets
+train_valid_dataset = torchvision.datasets.FashionMNIST(root=dataset_dir,
+                                           train=True,
+                                           transform= None, #transforms.ToTensor(),
+                                           download=True)
+
+# Split it into training and validation sets
+nb_train = int((1.0 - valid_ratio) * len(train_valid_dataset))
+nb_valid =  int(valid_ratio * len(train_valid_dataset))
+train_dataset, valid_dataset = torch.utils.data.dataset.random_split(train_valid_dataset, [nb_train, nb_valid])
+
+
+# Load the test set
+test_dataset = torchvision.datasets.FashionMNIST(root=dataset_dir,
+                                                 transform= None, #transforms.ToTensor(),
+                                                 train=False)
+                                                 
+                                                 
+ class DatasetTransformer(torch.utils.data.Dataset):
+
+    def __init__(self, base_dataset, transform):
+        self.base_dataset = base_dataset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img, target = self.base_dataset[index]
+        return self.transform(img), target
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+
+train_dataset = DatasetTransformer(train_dataset, transforms.ToTensor())
+valid_dataset = DatasetTransformer(valid_dataset, transforms.ToTensor())
+test_dataset  = DatasetTransformer(test_dataset , transforms.ToTensor())
+
+
+num_threads = 4     # Loading the dataset is using 4 CPU threads
+batch_size  = 128   # Using minibatches of 128 samples
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=True,                # <-- this reshuffles the data at every epoch
+                                          num_workers=num_threads)
+
+valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset,
+                                          batch_size=batch_size, 
+                                          shuffle=False,
+                                          num_workers=num_threads)
+
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False,
+                                          num_workers=num_threads)
+
+class LinearNet(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(LinearNet, self).__init__()
+        self.input_size = input_size
+        self.classifier = nn.Linear(self.input_size, num_classes)
+
+    def forward(self, x):
+        x = x.view(x.size()[0], -1)
+        y = self.classifier(x)
+        return y
+
+model = LinearNet(1*28*28, 10)
+
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+
+model.to(device)
+
+
+def train(model, loader, f_loss, optimizer, device):
+    """
+    Train a model for one epoch, iterating over the loader
+    using the f_loss to compute the loss and the optimizer
+    to update the parameters of the model.
+
+    Arguments :
+
+        model     -- A torch.nn.Module object
+        loader    -- A torch.utils.data.DataLoader
+        f_loss    -- The loss function, i.e. a loss Module
+        optimizer -- A torch.optim.Optimzer object
+        device    -- a torch.device class specifying the device
+                     used for computation
+
+    Returns :
+    """
+
+    # We enter train mode. This is useless for the linear model
+    # but is important for layers such as dropout, batchnorm, ...
+    model.train()
+
+    for i, (inputs, targets) in enumerate(loader):
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        # Compute the forward pass through the network up to the loss
+        outputs = model(inputs)
+        loss = f_loss(outputs, targets)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+# An example of calling train to learn over 10 epochs of the training set
+for i in range(10):
+    train(model, train_loader, f_loss, optimizer, device)
+    
+    
+def test(model, loader, f_loss, device):
+    """
+    Test a model by iterating over the loader
+
+    Arguments :
+
+        model     -- A torch.nn.Module object
+        loader    -- A torch.utils.data.DataLoader
+        f_loss    -- The loss function, i.e. a loss Module
+        device    -- The device to use for computation 
+
+    Returns :
+
+        A tuple with the mean loss and mean accuracy
+
+    """
+    # We disable gradient computation which speeds up the computation
+    # and reduces the memory usage
+    with torch.no_grad():
+        # We enter evaluation mode. This is useless for the linear model
+        # but is important with layers such as dropout, batchnorm, ..
+        model.eval()
+        N = 0
+        tot_loss, correct = 0.0, 0.0
+        for i, (inputs, targets) in enumerate(loader):
+
+            # We got a minibatch from the loader within inputs and targets
+            # With a mini batch size of 128, we have the following shapes
+            #    inputs is of shape (128, 1, 28, 28)
+            #    targets is of shape (128)
+
+            # We need to copy the data on the GPU if we use one
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            # Compute the forward pass, i.e. the scores for each input image
+            outputs = model(inputs)
+
+            # We accumulate the exact number of processed samples
+            N += inputs.shape[0]
+
+            # We accumulate the loss considering
+            # The multipliation by inputs.shape[0] is due to the fact
+            # that our loss criterion is averaging over its samples
+            tot_loss += inputs.shape[0] * f_loss(outputs, targets).item()
+
+            # For the accuracy, we compute the labels for each input image
+            # Be carefull, the model is outputing scores and not the probabilities
+            # But given the softmax is not altering the rank of its input scores
+            # we can compute the label by argmaxing directly the scores
+            predicted_targets = outputs.argmax(dim=1)
+            correct += (predicted_targets == targets).sum().item()
+        return tot_loss/N, correct/N
+      
+      
+      
+    for t in range(epochs):
+      print("Epoch {}".format(t))
+      train(model, train_loader, f_loss, optimizer, device)
+
+    val_loss, val_acc = test(model, valid_loader, f_loss, device)
+    print(" Validation : Loss : {:.4f}, Acc : {:.4f}".format(val_loss, val_acc))
