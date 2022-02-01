662b3dd33f66cb8df5995e711456aa008bd36180
lokilone <albanthuet@gmail.com>
2022-02-01 15:58:22 +0100

    Finish Preprocessing

--- Preprocessing.py
+++ Preprocessing.py
@@ -5,79 +5,61 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import numpy as np
 
+
 ###############################
 ##### Defining transforms #####
 ###############################
 
-
-params = {"padding": (300, 300)}
-
-# pad smaller images to desired size
-pad = torchvision.transforms.Pad(padding=params["padding"], fill=0)
-centercrop = torchvision.transforms.CenterCrop(300)  # Crop images to be of same 300x300
 # Images are B/W but in 3 channels, we only need one
 greyscale = torchvision.transforms.Grayscale(num_output_channels=1)
 # Negative
 invert = torchvision.transforms.functional.invert
 # Resize
 resize = torchvision.transforms.Resize((300,300))
+# Normalization
+normalization = torchvision.transforms.Normalize(mean=[0.0988],std=[0.1444])
 
 
 ### Data Augmentation
 rotate = torchvision.transforms.RandomRotation((0, 360))
-
-#augmentation = torchvision.transforms.AutoAugment()
-
-
-# Normalization
-normalization = torchvision.transforms.Normalize(mean=[0.13],std=[0.17])
+flip = torchvision.transforms.RandomHorizontalFlip(p=0.5)
+flou = torchvision.transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 1))
 
 
 # Compose transforms
-composed_transforms = torchvision.transforms.Compose([greyscale, invert, resize, torchvision.transforms.ToTensor(), normalization])
+test_composed_transforms = torchvision.transforms.Compose([greyscale, invert, resize, torchvision.transforms.ToTensor(), normalization])
 
-
-# Create transformer class
-class DatasetTransformer(torch.utils.data.Dataset):
-
-    def __init__(self, base_dataset, transforms):
-        self.base_dataset = base_dataset
-        self.transform = transforms
-
-    def __getitem__(self, index):
-        img, target = self.base_dataset[index]
-        return self.transform(img), target
-
-    def __len__(self):
-        return len(self.base_dataset)
-
+train_composed_transforms = torchvision.transforms.Compose([greyscale, invert, flip, rotate, resize, torchvision.transforms.ToTensor(), normalization])
 
 ########################
 ##### Loading Data #####
 ########################
-print(os.path.exists("/mounts/Datasets1/ChallengeDeep/train/"))
 valid_ratio = 0.2
 
-# Load learning data
-#train_path = "/mounts/Datasets1/ChallengeDeep/train/"
-train_path = "/usr/users/gpusdi1/gpusdi1_49/Bureau/sample_train"
+##### Load learning data
 
-print('data loading...')
-dataset = torchvision.datasets.ImageFolder(train_path, composed_transforms)
-print('data loaded')
+# Whole dataset
+train_path = "/opt/ChallengeDeep/train/"
+
+# Little sample to try
+#train_path = "/usr/users/gpusdi1/gpusdi1_49/Bureau/sample_train"
+
+print('data train loading...')
+dataset = torchvision.datasets.ImageFolder(train_path, train_composed_transforms)
+print('data train loaded')
+
+##### Load test data
+test_path = "/opt/ChallengeDeep/test/"
+
+#print('data test loading...')
+#dataset = torchvision.datasets.ImageFolder(train_path, valid_composed_transforms)
+#print('data test loaded')
 
 # Train test split
 nb_train = int((1.0 - valid_ratio) * len(dataset))
 nb_valid = len(dataset)-nb_train
-train_dataset, valid_dataset = torch.utils.data.dataset.random_split(dataset, [
-                                                                     nb_train, nb_valid])
+train_dataset, valid_dataset = torch.utils.data.dataset.random_split(dataset, [nb_train, nb_valid])
 print('data split')
-
-
-### Augmentation
-augmentation = torchvision.transforms.AutoAugment
-
-#train_dataset = augmentation(train_dataset)
 
 ##### Generating Loaders #####
 num_workers = 4
@@ -101,28 +83,62 @@ print("The train set contains {} images, in {} batches".format(
 print("The validation set contains {} images, in {} batches".format(
     len(valid_loader.dataset), len(valid_loader)))
 
-imgs = torch.stack([img_t for img_t, _ in valid_loader.dataset], dim=3)
-print(imgs.shape)
-print(imgs.view(1, -1).mean(dim=1))
-print(imgs.view(1, -1).std(dim=1))
+####################
+### Compute mean ###
+####################
+
+def compute_global_mean_std(loader):
+    print("Computing train data mean")
+    # Compute the mean over minibatches
+    mean_img = None
+    i = 0
+    num_img = 0
+    for imgs, _ in loader:
+        i += 1
+        if mean_img is None:
+            print("step1")
+            mean_img = torch.zeros_like(imgs[0])
+            print("step2")
+        print("{}/{}".format(i, len(loader)))
+        mean_img += imgs.sum(dim=0)
+        num_img += len(imgs)
+        print((mean_img/num_img).view(1,-1).mean())
+    mean_img /= len(loader.dataset)
+    real_mean = mean_img.view(1,-1).mean()
+
+    print("Computing train data std")
+    # Compute the std over minibatches
+    std_img = torch.zeros_like(mean_img)
+    for imgs, _ in loader:
+        std_img += ((imgs - mean_img)**2).sum(dim=0)
+    std_img /= len(loader.dataset)
+    std_img = torch.sqrt(std_img)
+
+    # Set the variance of pixels with no variance to 1
+    # Because there is no variance
+    # these pixels will anyway have no impact on the final decision
+    std_img[std_img == 0] = 1
+    real_std = std_img.view(1,-1).mean()
+
+    return real_mean, real_std
+
 
 #############################
 ##### Display Some data #####
 #############################
-n_samples = 30
+def display_data(n_samples):
+    class_names = dataset.classes
+    imgs, labels = next(iter(train_loader))
 
-class_names = dataset.classes
-imgs, labels = next(iter(train_loader))
+    fig = plt.figure(figsize=(30, 30), facecolor='w')
 
-fig = plt.figure(figsize=(30, 30), facecolor='w')
+    for col in range(n_samples):
+        ax = plt.subplot(2, n_samples, col+1)
+        plt.imshow(imgs[col, 0, :, :], vmin=0, vmax=1.0, cmap=cm.gray)
+        ax.set_title("{}".format(class_names[labels[0]]), fontsize=15)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
 
-for col in range(n_samples):
-    ax = plt.subplot(2, n_samples, col+1)
-    plt.imshow(imgs[col, 0, :, :], vmin=0, vmax=1.0, cmap=cm.gray)
-    ax.set_title("{}".format(class_names[labels[0]]), fontsize=15)
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-plt.savefig('plancton_alban.png', bbox_inches='tight')
-print('saved_images')
-plt.show()
+    plt.savefig('plancton_alban.png', bbox_inches='tight')
+    print('saved_images')
+    plt.show()
